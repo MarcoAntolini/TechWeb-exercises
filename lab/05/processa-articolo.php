7bc6bde2a33d7067ae7bd9e2db65906af3fdbfa6
MarcoAntolini <marco.antolini2001@gmail.com>
2022-11-18 10:40:56 +0100

    lab06

--- lab/05/processa-articolo.php
+++ lab/05/processa-articolo.php
@@ -6,16 +6,21 @@ if (!isUserLoggedIn()) {
 }
 
 if ($_POST["action"] == 1) {
-    //Controllo su esistenza
     //Inserisco articolo
     $titoloarticolo = $_POST["titoloarticolo"];
     $testoarticolo = $_POST["testoarticolo"];
     $anteprimaarticolo = $_POST["anteprimaarticolo"];
     $dataarticolo = date("Y-m-d");
     $autore = $_SESSION["idautore"];
-
+    $categorie = $dbh->getCategories();
+    $categorie_inserite = array();
+    foreach ($categorie as $categoria) {
+        if (isset($_POST["categoria_" . $categoria["idcategoria"]])) {
+            array_push($categorie_inserite, $categoria["idcategoria"]);
+        }
+    };
+    var_dump($categorie_inserite);
     list($result, $msg) = uploadImage(UPLOAD_DIR, $_FILES["imgarticolo"]);
-
     if ($result == 1) {
         $imgarticolo =  $msg;
         $id = $dbh->insertArticle($titoloarticolo, $testoarticolo, $anteprimaarticolo, $dataarticolo, $imgarticolo, $autore);
