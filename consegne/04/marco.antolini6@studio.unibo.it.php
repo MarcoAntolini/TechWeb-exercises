a44c672f0df5202beeb1c58fecb70bd7f9c44172
MarcoAntolini <marco.antolini2001@gmail.com>
2022-11-23 19:45:56 +0100

    .

--- consegne/04/marco.antolini6@studio.unibo.it.php
+++ consegne/04/marco.antolini6@studio.unibo.it.php
@@ -6,10 +6,23 @@ if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
-function hjk($db)
+function getSet($db, $var)
 {
-    $query = "";
+    $query = "SELECT COUNT(*) FROM insiemi WHERE insieme=?";
     $stmt = $db->prepare($query);
-    $stmt->bind_param();
+    $stmt->bind_param('i', $var);
     return $stmt->execute();
 }
+
+function checkValidity($db)
+{
+    if (isset($_GET['A']) && isset($_GET['B'])) {
+        if (getSet($db, $_GET['A']) != 0 && getSet($db, $_GET['B']) != 0) {
+            return 2;
+        }
+    }
+    return 3;
+}
+
+echo (getSet($db, $_GET['A']));
+echo (checkValidity($db));
