30cbeadaaf516722625b3556297ac307a5390371
MarcoAntolini <marco.antolini2001@gmail.com>
2022-11-24 02:22:01 +0100

    try

--- consegne/04/marco.antolini6@studio.unibo.it.php
+++ consegne/04/marco.antolini6@studio.unibo.it.php
@@ -6,23 +6,48 @@ if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
-function getSet($db, $var)
+function getCount($db, $var)
 {
-    $query = "SELECT COUNT(*) FROM insiemi WHERE insieme=?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param('i', $var);
-    return $stmt->execute();
+    $query = $db->prepare("SELECT COUNT(*) FROM insiemi WHERE insieme = ?");
+    $query->bind_param('i', $var);
+    $query->execute();
+    return $query->get_result()->fetch_row()[0];
 }
 
 function checkValidity($db)
 {
-    if (isset($_GET['A']) && isset($_GET['B'])) {
-        if (getSet($db, $_GET['A']) != 0 && getSet($db, $_GET['B']) != 0) {
-            return 2;
-        }
+    if (!isset($_GET['A'], $_GET['B'], $_GET['O'])) {
+        return false;
     }
-    return 3;
+    if (getCount($db, $_GET['A']) == 0) {
+        return false;
+    }
+    if (getCount($db, $_GET['B']) == 0) {
+        return false;
+    }
+    if (($_GET['O'] != 'i') or ($_GET['O'] != 'u')) {
+        return false;
+    }
+    return true;
 }
 
-echo (getSet($db, $_GET['A']));
-echo (checkValidity($db));
+function initArray($db, $var)
+{
+    $query = $db->prepare("SELECT * FROM insiemi WHERE insieme = ?");
+    $query->bind_param('i', $var);
+    $query->execute();
+    return $query->get_result()->fetch_all(MYSQLI_ASSOC);
+}
+
+function createNewArray($db)
+{
+    if ($_GET['O'] == 'u') {
+        $union = array_merge(initArray($db, $_GET['A']), initArray($db, $_GET['B']));
+        return $union;
+    } else if ($_GET['O'] == 'i') {
+        $intersection = array_intersect(initArray($db, $_GET['A']), initArray($db, $_GET['B']));
+        return $intersection;
+    }
+}
+
+var_dump(createNewArray($db));
