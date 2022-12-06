3c304e42abfc565859fd3adbf2df7d529569ec94
MarcoAntolini <marco.antolini2001@gmail.com>
2022-12-07 00:00:27 +0100

    .

--- consegne/05 - js/marcoantolini6@studio.unibo.it.js
+++ consegne/05 - js/marcoantolini6@studio.unibo.it.js
@@ -1,38 +1,49 @@
 var images = document.querySelectorAll("img");
 
-function loadPage() {
-    for (var i = 2; i < images.length; i++) {
-        images[i].style.visibility = "hidden";
-    }
-    images[0].className = "current";
-}
+images.forEach(image => addEventListener("click", function () {
+    image.forEach(img => img.removeAttribute("class"));
+    this.setAttribute("class", "current");
+    image.forEach(img => img.style.visibility = "hidden");
+    this.style.visibility = "visible";
+    this.previousElementSibling ? this.previousElementSibling.style.visibility = "visible" : null;
+    this.nextElementSibling ? this.nextElementSibling.style.visibility = "visible" : null;
+}));
 
-function imageClickHandler() {
-    for (var i = 0; i < images.length; i++) {
-        images[i].onclick = function () {
-            if (this.className != "current") {
-                this.className = "current";
-                this.style.visibility = "visible";
-                for (var j = 0; j < images.length; j++) {
-                    if (images[j] != this) {
-                        images[j].removeAttribute("class");
-                        images[j].style.visibility = "hidden";
-                    }
-                }
-                var p = this.previousElementSibling;
-                var n = this.nextElementSibling;
-                if (p) {
-                    p.style.visibility = "visible";
-                }
-                if (n) {
-                    n.style.visibility = "visible";
-                }
-            }
-        };
-    }
-}
+images[0].click();
 
-window.onload = function () {
-    loadPage();
-};
-imageClickHandler();
+// function loadPage() {
+//     for (var i = 2; i < images.length; i++) {
+//         images[i].style.visibility = "hidden";
+//     }
+//     images[0].className = "current";
+// }
+
+// function imageClickHandler() {
+//     for (var i = 0; i < images.length; i++) {
+//         images[i].onclick = function () {
+//             if (this.className != "current") {
+//                 this.className = "current";
+//                 this.style.visibility = "visible";
+//                 for (var j = 0; j < images.length; j++) {
+//                     if (images[j] != this) {
+//                         images[j].removeAttribute("class");
+//                         images[j].style.visibility = "hidden";
+//                     }
+//                 }
+//                 var p = this.previousElementSibling;
+//                 var n = this.nextElementSibling;
+//                 if (p) {
+//                     p.style.visibility = "visible";
+//                 }
+//                 if (n) {
+//                     n.style.visibility = "visible";
+//                 }
+//             }
+//         };
+//     }
+// }
+
+// window.onload = function () {
+//     loadPage();
+// };
+// imageClickHandler();
