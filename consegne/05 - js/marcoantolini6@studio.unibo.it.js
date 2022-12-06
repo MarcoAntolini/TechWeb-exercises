538e5a1a03cfbe1e5e919467c74573973fa37fc0
MarcoAntolini <marco.antolini2001@gmail.com>
2022-12-07 00:39:49 +0100

    final fix

--- consegne/05 - js/marcoantolini6@studio.unibo.it.js
+++ consegne/05 - js/marcoantolini6@studio.unibo.it.js
@@ -1,49 +1,12 @@
 var images = document.querySelectorAll("img");
 
-images.forEach(image => addEventListener("click", function () {
-    image.forEach(img => img.removeAttribute("class"));
+images.forEach(image => image.addEventListener("click", function () {
+    images.forEach(img => img.removeAttribute("class"));
     this.setAttribute("class", "current");
-    image.forEach(img => img.style.visibility = "hidden");
+    images.forEach(img => img.style.visibility = "hidden");
     this.style.visibility = "visible";
     this.previousElementSibling ? this.previousElementSibling.style.visibility = "visible" : null;
     this.nextElementSibling ? this.nextElementSibling.style.visibility = "visible" : null;
 }));
 
 images[0].click();
-
-// function loadPage() {
-//     for (var i = 2; i < images.length; i++) {
-//         images[i].style.visibility = "hidden";
-//     }
-//     images[0].className = "current";
-// }
-
-// function imageClickHandler() {
-//     for (var i = 0; i < images.length; i++) {
-//         images[i].onclick = function () {
-//             if (this.className != "current") {
-//                 this.className = "current";
-//                 this.style.visibility = "visible";
-//                 for (var j = 0; j < images.length; j++) {
-//                     if (images[j] != this) {
-//                         images[j].removeAttribute("class");
-//                         images[j].style.visibility = "hidden";
-//                     }
-//                 }
-//                 var p = this.previousElementSibling;
-//                 var n = this.nextElementSibling;
-//                 if (p) {
-//                     p.style.visibility = "visible";
-//                 }
-//                 if (n) {
-//                     n.style.visibility = "visible";
-//                 }
-//             }
-//         };
-//     }
-// }
-
-// window.onload = function () {
-//     loadPage();
-// };
-// imageClickHandler();
